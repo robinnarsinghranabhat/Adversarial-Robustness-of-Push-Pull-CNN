e5be612554ddc3489fbee89ebd0b7743e1b3e793
Robin Narsingh Ranabhat <robinnarsingha123@gmail.com>
2024-12-18 15:25:46 -0600

    geom transforms

--- utils/geometric_transforms.py
+++ utils/geometric_transforms.py
@@ -3,9 +3,9 @@ import torchvision.transforms as transforms
 def geometric_transforms(severity=1):
     # Severity controls the intensity of transformations
     return transforms.Compose([
-        transforms.RandomRotation(degrees=30 * severity),
+        # transforms.RandomRotation(degrees=30 * severity),
         transforms.RandomAffine(
-            degrees=0,
+            degrees=(-30 * severity, 30 * severity),
             translate=(0.1 * severity, 0.1 * severity),
             scale=(1 - 0.1 * severity, 1 + 0.1 * severity),
             shear=10 * severity
